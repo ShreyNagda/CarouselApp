0ccbd6fe9f2ddc6ad6d49d6b2eb29d921170f4e6
Shrey Nagda <shreynagda2714@gmail.com>
2023-09-28 19:26:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+// import { createClient } from "pexels";
+import "./App.css";
 
 function App() {
+  const API_KEY = "MqfKDtZmz5CfB9fpp1ooaDBUNs1y8kvP7iJ5UXumfyZMssEbr2ABYH3s";
+  // let images = [image1, image2, image3, image4, image5, image6];
+  const [loading, setLoading] = useState(true);
+  const [images, setImages] = useState([]);
+  const [photographers, setPhotographers] = useState([]);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const getPhotos = async () => {
+    await fetch("https://api.pexels.com/v1/curated?per_page=10", {
+      headers: { Authorization: API_KEY },
+    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        setLoading(false);
+        const photos = data.photos;
+        setImages(photos.map((photo) => photo.src.medium));
+        setPhotographers(photos.map((photo) => photo.photographer));
+      });
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  const nextSlide = () => {
+    setCurrentImage(currentImage === images.length - 1 ? 0 : currentImage + 1);
+  };
+  const prevSlide = () => {
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Image Carousel</h2>
+      <div className="slide-wrapper">
+        <button className="button" onClick={prevSlide}>
+          &lt;
+        </button>
+        {!loading && (
+          <div className="image-box">
+            <img className="image" src={images[currentImage]} alt="back" />
+          </div>
+        )}
+        <button className="button" onClick={nextSlide}>
+          &gt;
+        </button>
+      </div>
+      <div className="text">{photographers[currentImage]}</div>
     </div>
   );
 }
